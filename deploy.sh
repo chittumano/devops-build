161cbbe2c0deb18dfd7a494c0860b8d77b1633c0
mano <rkmctwo1@gmail.com>
2024-05-15 09:38:21 +0000

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh'chmod +x build.sh'
+    sh'./build.sh'
+    docker login -u rkmctwo1 -p $DOCKER_PASSWORD
+    docker tag react-nginx rkmctwo1/dev
+    docker push rkmctwo1/dev
+
+elif [[ $GIT_BRANCH == "origin/prod" ]]; then
+    sh'chmod +x build.sh'
+    sh'./build.sh'
+    docker login -u rkmctwo1 -p $DOCKER_PASSWORD
+    docker tag react-nginx rkmctwo1/prod 
+    docker push rkmctwo1/prod
+fi
