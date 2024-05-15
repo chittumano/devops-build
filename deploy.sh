d6b1593e9c53c2c492ec9c40553ec83ffe3fbcc3
chittumano <168729775+chittumano@users.noreply.github.com>
2024-05-15 15:21:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u rkmctwo1 -p $DOCKER_PASSWORD
-    docker tag react-nginx rkmctwo1/dev
+    docker tag react-capstone rkmctwo1/dev
     docker push rkmctwo1/dev
 
 elif [[ $GIT_BRANCH == "origin/prod" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u rkmctwo1 -p $DOCKER_PASSWORD
-    docker tag react-nginx rkmctwo1/prod 
+    docker tag react-capstone rkmctwo1/prod 
     docker push rkmctwo1/prod
 fi
